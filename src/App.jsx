a58271760b5a7b80bdf769d5827e9844bae9c007
bashar akash <akashh889007@gmail.com>
2023-10-09 16:26:35 +0600

    Password_genaretor

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import PassGen1 from "./Components/PassGen-1.jsx";
+import PassGen2 from "./Components/PassGen2.jsx";
+import PassGen3 from "./Components/PassGen3.jsx";
+
+function App() {
+    return (
+        <>
+            <PassGen1/>
+            <PassGen2/>
+            <PassGen3/>
+        </>
+    )
+}
+
+export default App
